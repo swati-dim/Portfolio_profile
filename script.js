4c5803c5b86984304c9b98701c8cc6faca8383de
swati-dim <163723381+swati-dim@users.noreply.github.com>
2024-03-22 23:20:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function scrolltop() {
+    let a = window.document;
+    a = location.href = '#home';
+}
+let contact = document.getElementById('contactForm');
+contact.addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert(`Sorry Request cannot be accepted due to some technical errors`);
+})
